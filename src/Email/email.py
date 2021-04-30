6e57127faf4f7f95f90fdb3677ce9664e338cf2c
d5b94396feba3 <sha.rah646@gmail.com>
2021-04-30 13:58:47 +0600

    adding web-app

--- src/Email/email.py
+++ src/Email/email.py
@@ -6,25 +6,18 @@ from src import app, mail, urlSTS
 
 
 def recipient_email():
-    # return current_user.email
-    return 'shahiburrahman55@gmail.com'
-
+    return 'Recipient Mail username here'
 
 def default_email():
-    return 'swap.mail.demo@gmail.com'  # mail username
-
+    return 'Sender Mail username here'  # mail will be sent from this mail username
 
 def send_contact_form(name,email,message):
 
     recipient = recipient_email()
     sender = default_email()
-    msg = Message('New Contact Form From ThyroidML', sender=sender, recipients=[recipient])
+    msg = Message('New Contact Form From BigMartAutoAI', sender=sender, recipients=[recipient])
     msg.html = render_template(
         'email/contact_form.html',name=name,email=email,message=message)
     mail.send(msg)
 
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
